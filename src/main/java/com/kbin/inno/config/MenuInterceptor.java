5996c45dab64cdcaaa11b7da8e5498dda2ea0f28
rhko <rhko@labpl.co.kr>
2025-02-06 10:50:25 +0900

    - 메뉴 관리 백엔드 처리 - 인터셉터 추가

---
+++ src/main/java/com/kbin/inno/config/MenuInterceptor.java
@@ -0,0 +1,36 @@
+package com.kbin.inno.config;
+
+import com.kbin.inno.Main.Service.MainService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MenuInterceptor implements HandlerInterceptor {
+
+    private final MainService mainService;
+
+    public MenuInterceptor(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        System.out.println(request.getRequestURI());
+        request.setAttribute("menuList", mainService.selectMenu());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
--- src/main/java/com/kbin/inno/config/WebConfig.java
+++ src/main/java/com/kbin/inno/config/WebConfig.java
@@ -1,38 +1,20 @@
 package com.kbin.inno.config;
 
-import java.util.List;
-
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 //public class WebConfig implements WebMvcConfigurer {
 public class WebConfig extends WebMvcConfigurerAdapter {
-	
+
+    private final MenuInterceptor menuInterceptor;
+
+    public WebConfig(MenuInterceptor menuInterceptor) {
+        this.menuInterceptor = menuInterceptor;
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		//registry.addResourceHandler("/kbinnovationhub/upload/**").addResourceLocations("file:///D:/fsfile/dev_kbinnovation/");
@@ -41,5 +23,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     	registry.addResourceHandler("/summernoteimages/**")
     	        .addResourceLocations("file:///D:/fsifle/dev_kbinnovation/");
                 //.addResourceLocations("file:/fsfile/dev_kbinnovation/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(menuInterceptor)
+                .addPathPatterns("/starters/**"
+                        , "/nurture/**"
+                        , "/error/**"
+                        , "/community/**"
+                        , "/"
+                        , "/startup/**")
+        ;
     }
 }
